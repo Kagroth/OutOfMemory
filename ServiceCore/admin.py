053bf7fa8bb2b6fad7c66213cfb761b5df724108
Kamil Bania <kamilbania2@gmail.com>
2019-04-16 18:42:24 +0200

    Dodanie utworzonych modeli do panelu admina

--- ServiceCore/admin.py
+++ ServiceCore/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(CV)
+admin.site.register(JobOffer)
+admin.site.register(Application)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Rating)
