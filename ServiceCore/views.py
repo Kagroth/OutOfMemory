77be6fb6b5554d3f53aadd56da4e4f4ca2471893
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-04-16 19:59:59 +0200

    LoginForm, RegisterForm

--- ServiceCore/views.py
+++ ServiceCore/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from ServiceCore.forms import LoginForm
+from ServiceCore.forms import LoginForm, RegisterForm
 
 
 def index(request):
-    return render(request, 'outofmemory/index.html')
+    return render(request, 'outofmemory/index.html') # strona glowna
 
 
 # widok logowania
@@ -35,3 +36,26 @@ def loginuser(request):
 def logoutuser(request):
     logout(request)
     return redirect('index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            email = request.POST.get('email')
+            try:
+                User.objects.create_user(username=email.split("@")[0], email=request.POST.get('email'), password=request.POST.get('password'), first_name=request.POST.get('firstName'), last_name=request.POST.get('lastName'))
+            except:
+                return render(request, 'outofmemory/index.html',
+                              {'message': 'Nie udało się zarejestrować'}) #sciezka
+
+            return render(request, 'outofmemory/index.html',
+                          {'message': 'Rejestracja przebiegła pomyślnie!'}) #sciezka
+        else:
+            return render(request, 'outofmemory/index.html',
+                          {'message': 'Podaj wszystie wymagane dane!'}) #sciezka
+
+    return index(request) #sciezka
