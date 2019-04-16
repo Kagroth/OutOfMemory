531c32fb68e60f3912eb603ca66eae52487116f5
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-04-16 19:40:28 +0200

    LoginForm, RegisterForm

--- ServiceCore/forms.py
+++ ServiceCore/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class RegisterForm(forms.Form):
-    firstName = forms.CharField(max_length=100)
-    lastName = forms.CharField(max_length=100)
+    firstName = forms.CharField(max_length=25)
+    lastName = forms.CharField(max_length=25)
     email = forms.EmailField()
     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
     repeat_password = forms.CharField(max_length=32, widget=forms.PasswordInput)
