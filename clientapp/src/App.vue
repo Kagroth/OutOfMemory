3c9742c7d29ab760153010b639d9d0813ccb4a64
Kamil Bania <kamilbania2@gmail.com>
2019-04-20 18:39:42 +0200

    Utworzenie komponentu ListingTab z tymczasowym tlem

--- clientapp/src/App.vue
+++ clientapp/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
     <main-nav></main-nav>
+    <b-container fluid class="content">
+      <b-row>
+        <b-col cols="auto">
+          <listing-tab></listing-tab>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Layout components
+import BContainer from 'bootstrap-vue/es/components/layout/container';
+import BRow from 'bootstrap-vue/es/components/layout/row';
+import BCol from 'bootstrap-vue/es/components/layout/container';
+
+// custom components
 import MainNav from './components/MainNav.vue';
+import ListingTab from './components/ListingTab.vue';
 
 export default {
   name: 'app',
@@ -17,11 +32,21 @@ export default {
     }
   },
   components: {
-    'main-nav': MainNav
+    'b-container': BContainer,
+    'b-row': BRow,
+    'b-col': BCol,
+    'main-nav': MainNav,
+    'listing-tab': ListingTab
   }
 }
 </script>
 
 <style scoped>
+.content {
 
+  position: absolute;
+    top: 10%;
+  
+  background-color: lightblue;
+}
 </style>
---
+++ clientapp/src/components/ListingTab.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-tabs>
+            <b-tab title="Posty">One</b-tab>
+            <b-tab title="Oferty pracy">Two</b-tab>
+        </b-tabs>
+    </div>
+</template>
+<script>
+// Tab components
+import BTabs from 'bootstrap-vue/es/components/tabs/tabs';
+import BTab from 'bootstrap-vue/es/components/tabs/tab';
+
+export default {
+    components: {
+        'b-tabs': BTabs,
+        'b-tab': BTab
+    }    
+}
+</script>
+<style scoped>
+
+</style>
