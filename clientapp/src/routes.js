3b6f06c253cc0b64ab267f6f0752a8dd412dd80a
Kamil Bania <kamilbania2@gmail.com>
2019-04-24 00:21:15 +0200

    Dodanie przycisku Dodaj posta oraz utworzenie komponentu PostCreator i dodanie trasy do widoku kreatora

--- clientapp/src/routes.js
+++ clientapp/src/routes.js
@@ -1,9 +1,11 @@
 import ListingTab from './components/ListingTab.vue'
 import LoginForm from './components/LoginForm.vue'
 import RegisterForm from './components/RegisterForm.vue'
+import PostCreator from './components/PostCreator.vue'
 
 export default [
     {path: '/', component: ListingTab},
     {path: '/login', name: 'Login', component: LoginForm},
-    {path: '/register', name: 'Register', component:RegisterForm}
+    {path: '/register', name: 'Register', component:RegisterForm},
+    {path: '/post/new', name: 'NewPost', component: PostCreator}
 ]
