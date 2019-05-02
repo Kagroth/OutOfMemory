4957ab8cf7c75494da45c908fc96ed3e0f65b432
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-05-02 17:52:41 +0200

    Dodano serializers.py

--- ServiceCore/urls.py
+++ ServiceCore/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-   # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     url(r'^register/$', views.CreateUserView.as_view(), name='user'),
 ]
