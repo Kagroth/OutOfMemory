757bd63de3adfafc765f6bd6a30c980a377fbfd5
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-05-02 18:49:54 +0200

    Zmodyfikowany urls.py i settings.py

--- ServiceCore/urls.py
+++ ServiceCore/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 from django.conf.urls import url, include
 
 from . import views
+from rest_framework import routers
+
+# from . import api
+
+
+#router = routers.DefaultRouter()
+#router.register(r'register', views.User, 'list')
 
 urlpatterns = [
+
     path('', views.index, name='index'),
+ #   url(r'^api/', include(router.urls)),
+  #  url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^register/$', views.CreateUserView.as_view(), name='user'),
 ]
