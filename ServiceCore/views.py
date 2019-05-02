9d8adfc395db21238f425d963f987b79bb70a3a3
Kamil Bania <kamilbania2@gmail.com>
2019-05-03 01:08:11 +0200

    Dodanie vue-resource i django-cors-headers, testowanie przesylania danych post do widoku z komponentu LoginForm

--- ServiceCore/views.py
+++ ServiceCore/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.cache import never_cache
+import json
 
 # Create your views here.
 
 def index(request):
     return render(request, 'servicecore/index.html')
+
+@csrf_exempt
+@never_cache
+def testApiCall(request):
+    content = {}
+    if request.method == "POST":
+        content = json.loads(request.body)
+
+    print(content)
+    return JsonResponse({"content": content})
--- outofmemory/settings.py
+++ outofmemory/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ServiceCore'
+    'ServiceCore',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080'
+)
