23accf90df8eb2fc452e6148c03867488fc3e694
wgalka <wgalka2@op.pl>
2019-05-04 02:54:46 +0200

    Snippet serialized tutorial working

--- outofmemory/settings.py
+++ outofmemory/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'corsheaders',
+    'rest_framework',
     'ServiceCore',
-    'corsheaders'
+    
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
