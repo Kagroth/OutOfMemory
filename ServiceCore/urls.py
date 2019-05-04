f4d4f8bb547a5472c952c8f324e18345f017dabb
wgalka <wgalka2@op.pl>
2019-05-04 02:54:46 +0200

    Snippet serialized tutorial working

--- ServiceCore/urls.py
+++ ServiceCore/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from snippets import views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.testApiCall, name='testApiCall'),
-    path('register/', views.testApiCall)
+    path('register/', views.testApiCall),
+    path('snippetstest/', views.snippet_list),
+    path('snippetstest/<int:pk>/', views.snippet_detail),
 ]
