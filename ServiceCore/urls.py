2af186eb66908bcec25e56bf788cffc625892925
Kamil Bania <kamilbania2@gmail.com>
2019-05-08 01:19:36 +0200

    Dodanie widokow odpowiedzialnych za generowanie tokena

--- ServiceCore/urls.py
+++ ServiceCore/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.conf.urls import include, url
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,5 +14,6 @@ urlpatterns = [
     path('snippetstest/<int:pk>/', views.snippet_detail),
     path('user/', views.ProfileRecordView.as_view()),
     path('post/', views.PostView.as_view()),
-   #path('hello/', views.HelloView.as_view())
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
