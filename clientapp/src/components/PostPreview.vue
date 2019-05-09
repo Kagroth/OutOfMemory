76b54d4093fc828bd9e3c17a0d07d5b04e46b682
Kamil Bania <kamilbania2@gmail.com>
2019-05-09 18:44:33 +0200

    Polaczenie props do jednego postPreview

--- clientapp/src/components/PostPreview.vue
+++ clientapp/src/components/PostPreview.vue
@@ -1,28 +1,26 @@
 <template>
     <div class="mb-1">
-        <b-card v-bind:title="title">            
+        <b-card v-bind:title="postPreview.title">            
             <b-card-text>
                 <div class="cardAuthor w-75">
-                    <span>Autor: {{ author }} </span>
+                    <span>Autor: {{ postPreview.author }} </span>
                 </div>
                 <div class="cardBadges w-25">
-                    <span class="mr-5">Wyswietlen: {{ viewsCount}} </span><br>
-                    <span class="mr-5">Odpowiedzi: {{ answersCount }} </span>
+                    <span class="mr-5">Wyswietlen: {{ postPreview.viewsCount}} </span><br>
+                    <span class="mr-5">Odpowiedzi: {{ postPreview.answersCount }} </span>
                 </div>
             </b-card-text>
             <b-card-text>
-                <b-badge variant="primary" class="mr-1" :key="tagName" v-for="tagName in tags">{{ tagName.tagName }}</b-badge>
+                <b-badge variant="primary" class="mr-1" :key="tagName" v-for="tagName in postPreview.tags">{{ tagName }}</b-badge>
             </b-card-text>            <!-- tymczasowe rozwiazanie -->
-            <div slot="footer"><small>{{ creationDate.slice(0, 10) }}</small></div>
+            <div slot="footer"><small>{{ postPreview.creationDate }}</small></div>
         </b-card>
     </div>    
 </template>
 
 <script>
 export default {
-    props: ['author', 'creationDate',
-            'viewsCount', 'answersCount',
-            'title', 'tags'],
+    props: ['postPreview'],
 
     data(){
         return {
