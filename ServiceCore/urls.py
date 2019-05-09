8b76b9b02b8fd359cd851da8cc85538822e31487
Kamil Bania <kamilbania2@gmail.com>
2019-05-09 16:51:49 +0200

    Paths - widok testowy hello oraz post/new i search

--- ServiceCore/urls.py
+++ ServiceCore/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls import include, url
 from . import views
+from .views import HelloView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,6 +15,9 @@ urlpatterns = [
     path('snippetstest/<int:pk>/', views.snippet_detail),
     path('user/', views.ProfileRecordView.as_view()),
     path('post/', views.PostView.as_view()),
+    path('post/search/', views.PostViewFilter.as_view()),
+    path('post/new/', views.PostCreate.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', HelloView.as_view()),
 ]
