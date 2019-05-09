139af88b8541b0b2b451ce807c6b13b982ca096d
Kamil Bania <kamilbania2@gmail.com>
2019-05-09 18:07:48 +0200

    Instalacja Vuex

--- clientapp/src/main.js
+++ clientapp/src/main.js
@@ -2,12 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import VueResource from 'vue-resource'
 import Routes from './routes'
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
+Vue.use(Vuex);
 Vue.use(VueResource);
+
+
+const store = new Vuex.Store({
+  state: {
+
+  },
+
+  mutations: {
+
+  },
+
+  actions: {
+    
+  }
+});
 
 const router = new VueRouter({
   routes: Routes,
