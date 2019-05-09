5755e75a5fc7fb39c5e6c5907fa72f5eba05a68e
Kamil Bania <kamilbania2@gmail.com>
2019-05-09 18:44:05 +0200

    Computed posts_previews, uproszczenie template

--- clientapp/src/components/ListingTab.vue
+++ clientapp/src/components/ListingTab.vue
@@ -9,26 +9,10 @@
         </b-row>
         <b-row class="mt-2">
           <b-col cols=12>
-            <!--
             <post-preview :key="post_preview"
-              v-for="post_preview in post_previews"
-              v-bind:author="post_preview.author"
-              v-bind:creationDate="post_preview.creationDate"    
-              v-bind:viewsCount="post_preview.viewsCount"
-              v-bind:answersCount="post_preview.answersCount"
-              v-bind:title="post_preview.title"
-              v-bind:tags="post_preview.tags"
-          ></post-preview>
-          -->
-          <post-preview :key="post_preview"
-            v-for="post_preview in posts_prevs"
-            v-bind:author="post_preview.author"
-            v-bind:creationDate="post_preview.createdAt"
-            v-bind:viewsCount="post_preview.viewsCount"
-            v-bind:answersCount="post_preview.comments.length"
-            v-bind:title="post_preview.title"
-            v-bind:tags="post_preview.tags">
-          </post-preview>
+              v-for="post_preview in posts_previews"
+              v-bind:postPreview="post_preview">
+            </post-preview>
           </b-col>          
         </b-row>
       </b-tab>
@@ -42,17 +26,25 @@ import api from "../api.js";
 import PostPreview from "./PostPreview.vue";
 
 export default {
+  components: {
+    'post-preview': PostPreview
+  },
 
-  props: ['post_previews'],
-  
   data() {
     return {
-      posts_prevs: []
-    };
+
+    }
+  },
+
+  computed: {
+    posts_previews() {
+      return this.$store.state.posts_previews;
+    }
   },
 
   created() {
     console.log(); 
+    console.log(this.$store);
 
     this.$http.get(api.getPostEndpoint(), {headers: {Authorization: "Bearer " + localStorage.getItem('token')}}).then(
       // on success
