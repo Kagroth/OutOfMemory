236a1936024dd2a997487aa587d2e922dc5c371a
Kamil Bania <kamilbania2@gmail.com>
2019-05-09 18:43:35 +0200

    Przeniesienie danych testowych z App do main.js

--- clientapp/src/main.js
+++ clientapp/src/main.js
@@ -15,14 +15,48 @@ Vue.use(VueResource);
 const store = new Vuex.Store({
   state: {
     username: null,    
-    isLoggedUser: (localStorage.getItem('token') === null),
+    //isLoggedUser: (localStorage.getItem('token') === null),
 
     posts: [
 
     ],
 
     posts_previews: [
+      {
+        author: "pjoterN",
+        creationDate: "20.04.2019",
+        viewsCount: 5,
+        answersCount: 1,
+        title: "Mam problem z NullPointerException",
+        tags: ["Java", "MySQL"]
+      },
 
+      {
+        author: "saszka",
+        creationDate: "20.04.2019",
+        viewsCount: 3,
+        answersCount: 0,
+        title: "Jak wysrodkowac diva w CSS",
+        tags: ["HTML", "CSS"]
+      },
+
+      {
+        author: "mariusz",
+        creationDate: "19.04.2019",
+        viewsCount: 23,
+        answersCount: 7,
+        title: "Nie dzialaja mi props w Vue.js",
+        tags: ["Javascript", "Vue", "Frontend"]
+      },
+
+      {
+        author: "mariusz",
+        creationDate: "16.04.2019",
+        viewsCount: 17,
+        answersCount: 3,
+        title: "Jak zrobic czary mary w Springu",
+        tags: ["Java", "Spring"]
+      }
     ],
 
   },
@@ -44,5 +78,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   render: h => h(App),
-  router: router
+  router: router,
+  store: store,
 })
