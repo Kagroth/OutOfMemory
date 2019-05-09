5f7cf5da2d58b02c78da5a7d3fe308f668914cfd
Kamil Bania <kamilbania2@gmail.com>
2019-05-09 16:47:20 +0200

    Zmiana odwolania do pola tagName obiektu tagName

--- clientapp/src/components/PostPreview.vue
+++ clientapp/src/components/PostPreview.vue
@@ -11,7 +11,7 @@
                 </div>
             </b-card-text>
             <b-card-text>
-                <b-badge variant="primary" class="mr-1" :key="tagName" v-for="tagName in tags">{{ tagName }}</b-badge>
+                <b-badge variant="primary" class="mr-1" :key="tagName" v-for="tagName in tags">{{ tagName.tagName }}</b-badge>
             </b-card-text>            <!-- tymczasowe rozwiazanie -->
             <div slot="footer"><small>{{ creationDate.slice(0, 10) }}</small></div>
         </b-card>
