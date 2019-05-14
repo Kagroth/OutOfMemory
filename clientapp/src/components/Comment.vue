5d78369ddacce2805fc2a4f83c3047956b9eda3d
Kamil Bania <kamilbania2@gmail.com>
2019-05-15 01:18:04 +0200

    Utworzenie komponentu komentarza

---
+++ clientapp/src/components/Comment.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-col cols=10 offset=1 class="border">
+        <b-row>
+            <b-col class="text-justify">{{ comment.commentField }}</b-col>
+            <b-col cols=1 class="text-center mt-1">
+                <b-button variant="success" size="sm" class="w-100">+</b-button><br>
+                <span>15</span><br>
+                <b-button variant="danger" size="sm" class="w-100">-</b-button>                
+            </b-col>
+        </b-row>
+        <b-row class="mt-1">
+            <b-col>Autor: {{ comment.author }} <br> {{ comment.createdAt.substring(0, 10) }}</b-col>
+        </b-row>
+    </b-col>
+</template>
+
+<script>
+export default {
+    props: ['comment']
+}
+</script>
+
+<style scoped>
+
+</style>
