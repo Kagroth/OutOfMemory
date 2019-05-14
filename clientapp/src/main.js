cc6898e9b91e7d92f789a84df4b0bca15f0c7e57
Kamil Bania <kamilbania2@gmail.com>
2019-05-14 15:30:02 +0200

    Zamiana vue-resource na axios, utworzenie obiektu Store

--- clientapp/src/main.js
+++ clientapp/src/main.js
@@ -5,70 +5,128 @@ import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import VueResource from 'vue-resource'
 import Routes from './routes'
+import api from './api'
+import axios from 'axios'
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 Vue.use(Vuex);
-Vue.use(VueResource);
-
 
 const store = new Vuex.Store({
   state: {
-    username: null,    
-    //isLoggedUser: (localStorage.getItem('token') === null),
+    token: localStorage.getItem('token'),
+    isLogged: (localStorage.getItem('token') !== null),
 
-    posts: [
-
-    ],
-
-    posts_previews: [
-      {
-        author: "pjoterN",
-        creationDate: "20.04.2019",
-        viewsCount: 5,
-        answersCount: 1,
-        title: "Mam problem z NullPointerException",
-        tags: ["Java", "MySQL"]
-      },
-
-      {
-        author: "saszka",
-        creationDate: "20.04.2019",
-        viewsCount: 3,
-        answersCount: 0,
-        title: "Jak wysrodkowac diva w CSS",
-        tags: ["HTML", "CSS"]
-      },
-
-      {
-        author: "mariusz",
-        creationDate: "19.04.2019",
-        viewsCount: 23,
-        answersCount: 7,
-        title: "Nie dzialaja mi props w Vue.js",
-        tags: ["Javascript", "Vue", "Frontend"]
-      },
-
-      {
-        author: "mariusz",
-        creationDate: "16.04.2019",
-        viewsCount: 17,
-        answersCount: 3,
-        title: "Jak zrobic czary mary w Springu",
-        tags: ["Java", "Spring"]
-      }
-    ],
+    post_previews: []
 
   },
 
   mutations: {
+    setToken (state, payload) {
+      localStorage.setItem('token', payload.token);
+      state.token = localStorage.getItem('token');
+      state.isLogged = (localStorage.getItem('token') != null)
+    },
 
+    setUsers (state, payload) {
+      state.users = payload
+    },
+
+    setPostPreviews (state, payload) {
+      console.log(payload)
+      state.post_previews = payload;
+    },
+
+    logout (state) {
+      localStorage.setItem('token', null);
+      state.token = null;
+      state.isLogged = false;
+    }
   },
 
   actions: {
-    
+    createUser ({commit}, payload) {
+      console.log("Wysylam request rejestracji")
+
+      axios.post(api.getRegisterEndpoint(), payload)
+           .then(response => console.log("Sukces" + response))
+           .catch(error => console.log(error.response))
+    },
+
+    loginUser ({commit}, payload) {
+      console.log("Wysylam request logowania");
+
+      return new Promise((resolve, reject) => {
+        axios.post(api.getLoginEndpoint(), payload)
+           .then(response => {
+             console.log(response.data.access);
+             commit('setToken', {
+               token: response.data.access
+             });
+             
+             resolve()
+           })
+           .catch(error => {            
+             console.log(error.response);
+             reject()
+           })
+      })      
+    },
+
+    getAllUsers({commit}, payload) {
+      console.log("Wysylam zadanie pobrania userow!");
+
+      return new Promise((resolve, reject) => {
+        axios.get('http://localhost:8000/users/')
+             .then((response) => {
+               commit('setUsers', response.data)
+               resolve();
+             })
+             .catch(() => {
+               console.log("Blad pobierania userow");
+               reject();
+             })
+      })
+    },
+
+    getAllPostPreviews ({commit}, payload) {
+      console.log("Wysylam zadanie wyswietlenia post previews!")
+
+      return new Promise((resolve, reject) => {
+        let authHeader = "Bearer " + this.state.token;
+        axios.get(api.getPostEndpoint(), {headers: {
+          'Authorization': authHeader
+        }})
+           .then((response) => {
+              commit('setPostPreviews', response.data)
+              resolve()
+           })
+           .catch(() => {
+            alert("Blad pobierania postow")
+            reject()
+           })
+      })      
+    },
+
+    createGroup ({commit}, payload) {
+      return new Promise((resolve, reject) => {
+        let authHeader = "Bearer " + this.state.token;
+
+        axios.post("http://localhost:8000/groups/", {
+          params: payload},
+          {headers: {
+          'Authorization': authHeader
+        }})
+          .then((response) => {
+            resolve()
+          })
+          .catch((error) => {
+            reject()
+          })
+      })
+    }
   }
-});
+})
 
 const router = new VueRouter({
   routes: Routes,
