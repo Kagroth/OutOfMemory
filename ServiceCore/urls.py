dcd138a0e7ff486a78626ce55422177d968c863e
Kamil Bania <kamilbania2@gmail.com>
2019-05-14 16:17:34 +0200

    Usuniecie testowych url

--- ServiceCore/urls.py
+++ ServiceCore/urls.py
@@ -8,16 +8,10 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.testApiCall, name='testApiCall'),
-    path('register/', views.testApiCall),
-    path('snippetstest/', views.snippet_list),
-    path('snippetstest/<int:pk>/', views.snippet_detail),
     path('user/', views.ProfileRecordView.as_view()),
     path('post/', views.PostView.as_view()),
     path('post/search/', views.PostViewFilter.as_view()),
     path('post/new/', views.PostCreate.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('hello/', HelloView.as_view()),
 ]
