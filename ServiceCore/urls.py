cf825a4c7b0466eaa26a7b8b83cf7d18f6ad1a63
Kamil Bania <kamilbania2@gmail.com>
2019-05-14 20:33:24 +0200

    Dodanie url do szczegolow posta

--- ServiceCore/urls.py
+++ ServiceCore/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('post/', views.PostView.as_view()),
     path('post/search/', views.PostViewFilter.as_view()),
     path('post/new/', views.PostCreate.as_view()),
+    path('post/details/<int:pk>', views.PostDetailsView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
