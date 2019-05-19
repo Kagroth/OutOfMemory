5eab306415d663fae14b0038adbd7af0de2a6cc9
Kamil Bania <kamilbania2@gmail.com>
2019-05-20 01:43:30 +0200

    Podpiecie comment.rate, wyswietlanie przyciskow gdy jest zalogowany, dodanie handlera dla przyciskow + i -

--- clientapp/src/components/Comment.vue
+++ clientapp/src/components/Comment.vue
@@ -2,10 +2,13 @@
     <b-col cols=10 offset=1 class="border">
         <b-row>
             <b-col class="text-justify">{{ comment.commentField }}</b-col>
-            <b-col cols=1 class="text-center mt-1">
-                <b-button variant="success" size="sm" class="w-100">+</b-button><br>
-                <span>15</span><br>
-                <b-button variant="danger" size="sm" class="w-100">-</b-button>                
+            <b-col cols=1 class="text-center mt-1" v-if="isLogged">
+                <b-button variant="success" size="sm" class="w-100" @click="rateComment(1)">+</b-button><br>
+                <span> {{ comment.rate }} </span><br>
+                <b-button variant="danger" size="sm" class="w-100" @click="rateComment(-1)">-</b-button>                
+            </b-col>
+            <b-col cols=1 v-else class="text-center mt-1">
+                <span> {{ comment.rate }} </span>
             </b-col>
         </b-row>
         <b-row class="mt-1">
@@ -16,7 +19,29 @@
 
 <script>
 export default {
-    props: ['comment']
+    props: ['comment'],
+
+    computed: {
+        isLogged () {
+            return this.$store.state.isLogged;
+        }
+    },
+
+    methods: {
+        rateComment (param) {             
+            console.log(param);
+            console.log(this.comment.pk);
+
+            this.$store.dispatch('rateComment', {
+                commentPk: this.comment.pk,
+                rate: param
+            }).then(
+                () => {console.log("Dodano ocene!");}
+            ).catch(
+                () => {console.log("Niedodano oceny");}
+            );
+        }
+    }
 }
 </script>
 
