ea0bce7a0c24789a162f0e4a89658d76f6703e5e
Kamil Bania <kamilbania2@gmail.com>
2019-05-24 22:12:07 +0200

    Dodanie pola company do wpisow doswiadczenia. dodanie pola isEditing, utworzenie pustych metod created i saveCV

--- clientapp/src/components/CV.vue
+++ clientapp/src/components/CV.vue
@@ -36,9 +36,10 @@
                                         <b-button variant="danger" size="sm" @click="removeExperience(index)">Usuń</b-button>
                                     </b-col>
                                 </b-row>                            
-                                <b-form-input v-model="exp.startDate" :type="date"></b-form-input>
-                                <b-form-input v-model="exp.endDate" :type="date"></b-form-input>
-                                <b-form-textarea v-model="exp.description"></b-form-textarea>                            
+                                <b-form-input v-model="exp.startDate" :type="date" placeholder="MM-RRRR"></b-form-input>
+                                <b-form-input v-model="exp.endDate" :type="date" placeholder="MM-RRRR"></b-form-input>                                
+                                <b-form-input v-model="exp.company" :type="text" placeholder="Nazwa firmy"></b-form-input>
+                                <b-form-textarea v-model="exp.description" placeholder="Opisz czym się zajmowałeś"></b-form-textarea>                            
                             </div>
                             <b-button variant="primary" size="sm" @click="addNewExperience" class="mt-2">Dodaj nowy wpis</b-button>
                         </b-form-group>
@@ -46,10 +47,10 @@
                         <b-form-group label="Twoje umiejętności:">
                             <div v-for="(skill, index) in skills" :key="(skill, index)" class="mt-2">
                                 <b-row>
-                                    <b-col cols=11 style="background-color: red;">
+                                    <b-col cols=11>
                                         <span>Wpis nr: {{ index + 1}}</span>
                                     </b-col>
-                                    <b-col cols=1 style="background-color: blue;">
+                                    <b-col cols=1>
                                         <b-button variant="danger" size="sm" @click="removeSkill(index)">Usuń</b-button>
                                     </b-col>                           
                                 </b-row>
@@ -64,7 +65,7 @@
             <b-row>
                 <b-col>
                     <hr>
-                    <b-button variant="success" size="sm">Zapisz</b-button>
+                    <b-button variant="success" size="sm" @click="saveCV">Zapisz</b-button>
                 </b-col>
             </b-row>
         </div>          
@@ -79,6 +80,7 @@ export default {
             hasCV: false,
             showCreator: false,
             isCreating: false,
+            isEditing: false,
             skills: [],
             experiences: [],
             levels: [
@@ -88,6 +90,12 @@ export default {
                 'Master'
             ]
         }
+    },
+
+    created(){
+        /*
+            Inicjalizacja parametrow: hasCV, showCreator, isCreating, isEditing
+        */
     },
 
     methods: {
@@ -100,6 +108,7 @@ export default {
             this.experiences.push({
                 startDate: "",
                 endDate: "",
+                company: "",
                 description: ""
             })
         },
@@ -117,6 +126,12 @@ export default {
 
         removeSkill(index) {
             this.skills.splice(index, 1)
+        },
+
+        saveCV() {
+            /*
+                Zapis CV do bazy
+            */
         }
     }
 }
