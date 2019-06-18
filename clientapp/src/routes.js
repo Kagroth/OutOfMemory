08d327fd1dbcb02c8fcabc6e0f03c09af540034b
Kamil Bania <kamilbania2@gmail.com>
2019-06-18 13:57:57 +0200

    Dodanie trasy, komponentu i zdarzenia wyswietlajacego profil innego uzytkownika

--- clientapp/src/routes.js
+++ clientapp/src/routes.js
@@ -10,7 +10,8 @@ import Profile from './components/Profile.vue'
 import CV from './components/CV.vue'
 import JobOfferForm from "./components/JobOfferForm"
 import JobOfferDetails from "./components/JobOfferDetails"
-import JobOfferEditForm from "./components/JobOfferEditForm";
+import JobOfferEditForm from "./components/JobOfferEditForm"
+import UserProfile from './components/UserProfile'
 
 export default [
   {path: '/', component: ListingTab},
@@ -20,6 +21,7 @@ export default [
   {path: '/post/:pk', name: 'PostDetails', component: PostDetails},
   {path: '/profile', name: 'Profile', component: Profile},
   {path: '/profile/cv', name: "CV", component: CV},
+  {path: '/profile/:username', name: 'UserProfile', component: UserProfile},
   {path: '/job/new', name: "NewJobOffer", component: JobOfferForm},
   {path: '/job/:pk', name: 'JobOfferDetails', component: JobOfferDetails},
   {path: '/job/edit/:pk', name: 'JobOfferEdit', component: JobOfferEditForm}
