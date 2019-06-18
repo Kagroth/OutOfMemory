664f1b46b6266024f69dbb40d837228c45bd7324
wgalka <wgalka2@op.pl>
2019-06-18 21:06:46 +0200

    zmiana strony po edycji oferty

--- clientapp/src/components/JobOfferEditForm.vue
+++ clientapp/src/components/JobOfferEditForm.vue
@@ -68,19 +68,18 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-button @click="updateJobOffer" type="submit" variant="primary">Edytuj ofertę</b-button>
+      <b-button @click="updateJobOffer" variant="primary">Edytuj ofertę</b-button>
     </b-form>
-<!--    <b-card class="mt-3" header="Form Data Result">-->
-<!--      <pre class="m-0">{{ jobOffer }}</pre>-->
-<!--    </b-card>-->
+    <!--    <b-card class="mt-3" header="Form Data Result">-->
+    <!--      <pre class="m-0">{{ jobOffer }}</pre>-->
+    <!--    </b-card>-->
   </div>
 </template>
 
 <script>
   export default {
     data() {
-      return {
-      }
+      return {}
     },
     created() {
       this.$store.dispatch('getJobOfferDetails', this.$route.params.pk)
@@ -105,8 +104,8 @@
           console.log("Edytowano ofertę pracy")
           alert("Edytowano ofertę pracy")
           console.log("wczytuje nowa scieżke")
+          this.$router.push("/job/"+this.jobOffer.pk)
           alert("nowa sciezka")
-          this.$router.push("/")
         });
       }
     },
